859b4c8f3c19ef6e5350a3cb214aaa90145350f9
Mathias Rav <rav@cs.au.dk>
2016-08-15 13:23:17 +0200

    Add list_display to ModelAdmins

--- lunchclub/admin.py
+++ lunchclub/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from lunchclub.models import Person, Attendance, Expense, AccessToken
 
-admin.site.register(Person)
-admin.site.register(Attendance)
-admin.site.register(Expense)
-admin.site.register(AccessToken)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('username', 'balance', 'user', 'created_time')
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('person', 'date', 'created_by', 'created_time')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('person', 'amount', 'date', 'created_by', 'created_time')
+
+
+class AccessTokenAdmin(admin.ModelAdmin):
+    list_display = ('person', 'created_time', 'use_count')
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(AccessToken, AccessTokenAdmin)
