d021f23ee6e7c92b12541b6bdd0275f088594e01
Mathias Rav <rav@cs.au.dk>
2017-07-15 09:22:31 +0200

    Refactor

--- lunchclub/admin.py
+++ lunchclub/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
-from lunchclub.models import Person, Attendance, Expense, AccessToken
+from lunchclub.models import (
+    Person, Attendance, Expense, AccessToken,
+)
 
 
+@admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('username', 'balance', 'user', 'created_time')
 
 
+@admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('person', 'date', 'created_by', 'created_time')
 
 
+@admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('person', 'amount', 'date', 'created_by', 'created_time')
 
 
+@admin.register(AccessToken)
 class AccessTokenAdmin(admin.ModelAdmin):
     list_display = ('person', 'created_time', 'use_count')
-
-
-admin.site.register(Person, PersonAdmin)
-admin.site.register(Attendance, AttendanceAdmin)
-admin.site.register(Expense, ExpenseAdmin)
-admin.site.register(AccessToken, AccessTokenAdmin)
