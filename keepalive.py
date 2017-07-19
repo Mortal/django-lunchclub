bf46266c8774b226afc846fe211ebca60584be5a
Mathias Rav <rav@cs.au.dk>
2017-07-19 10:01:30 +0200

    Fix keepalive

--- keepalive.py
+++ keepalive.py
@@ -4,7 +4,7 @@ import subprocess
 
 while True:
     subprocess.check_call(
-        ('python', '-c', 'import django; django.setup(); ' +
+        ('venv/bin/python', '-c', 'import django; django.setup(); ' +
          'import lunchclub.today; lunchclub.today.send_keepalive()'))
     print(datetime.datetime.now())
     time.sleep(30)
