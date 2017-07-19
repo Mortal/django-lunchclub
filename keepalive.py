ea7da105817c97d54bcb62b11fb40cbc5e828af6
Mathias Rav <rav@cs.au.dk>
2017-07-19 10:09:19 +0200

    Flush output line

--- keepalive.py
+++ keepalive.py
@@ -10,5 +10,5 @@ while True:
     subprocess.check_call(
         ('venv/bin/python', '-c', 'import django; django.setup(); ' +
          'import lunchclub.today; lunchclub.today.send_keepalive()'))
-    print(datetime.datetime.now())
+    print(datetime.datetime.now(), flush=True)
     time.sleep(30)
